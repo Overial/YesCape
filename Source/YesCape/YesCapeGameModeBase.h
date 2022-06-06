174b36f201884db71129120abe3742718f643053
Overial <dfjx2201@yandex.ru>
2022-06-06 21:36:38 +0300

    Version 1.0

---
+++ Source/YesCape/YesCapeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "YesCapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class YESCAPE_API AYesCapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
